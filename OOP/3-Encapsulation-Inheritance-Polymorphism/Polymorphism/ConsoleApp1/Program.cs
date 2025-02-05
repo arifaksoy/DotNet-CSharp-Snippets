745c7245882b4a2416d53c92becfa8ee21acf58d
Arif Aksoy <a.aksoy@cellsol.net>
2025-02-06 02:02:43 +0300

    Added polymorphism project

---
+++ OOP/3-Encapsulation-Inheritance-Polymorphism/Polymorphism/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Animal
+{
+    public virtual void MakeSound()  // virtual metod
+    {
+        Console.WriteLine("Animal sound");
+    }
+}
+
+class Dog : Animal
+{
+    public override void MakeSound()  // override edilen metod
+    {
+        Console.WriteLine("Woof Woof");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();
+        myAnimal.MakeSound();  // Animal sound
+
+        Animal myDog = new Dog();  // Polymorphism: Referans tipi Animal, nesne tipi Dog
+        myDog.MakeSound();  // Woof Woof
+    }
+}
