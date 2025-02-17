64440f91b3a157b4eaef7a4b006bb895575952d1
Arif Aksoy <a.aksoy@cellsol.net>
2025-02-17 15:54:55 +0300

    Added single responsibility princible

---
+++ Design Patterns/4-SOLID Principle/1-Single Responsibility Princible/SingleResponsibilityPrincible/Program.cs
@@ -0,0 +1,48 @@
+﻿var mybook = new Book { Title="Dive Into Design pattern",Author="Alexander S.", Price=30};
+
+var formattedBook = BookFormatter.DisplayBookInfo(mybook);
+Console.WriteLine(formattedBook);
+
+var discountedPrice = PriceCalculator.CalculateDisCountedPrice(mybook);
+Console.WriteLine(discountedPrice);
+
+
+public class NonSingleResponsiblePrincibleCompany
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Price { get; set; }
+
+    public string DisplayBookInfo() //get and format book info 
+    {
+       return this.Title + "book";
+    }
+
+    public string CalculateDisCountedPrice()
+    {
+        return "price: " + (this.Price + 20).ToString();
+    }
+}
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Price { get; set; }
+}
+
+public class BookFormatter
+{
+    public static string DisplayBookInfo(Book book) //get and format book info 
+    {
+        return book.Title + " book";
+    }
+}
+
+public class PriceCalculator
+{
+    public static string CalculateDisCountedPrice(Book book)
+    {
+        return"price: " + (book.Price + 20).ToString();
+    }
+}
