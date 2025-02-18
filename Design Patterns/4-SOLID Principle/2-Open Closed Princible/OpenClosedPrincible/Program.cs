789a752e3869bbfe32899c91e85c1a265a2e9f5e
Arif Aksoy <a.aksoy@cellsol.net>
2025-02-18 13:44:24 +0300

    Added open closed princible

---
+++ Design Patterns/4-SOLID Principle/2-Open Closed Princible/OpenClosedPrincible/Program.cs
@@ -0,0 +1,76 @@
+﻿var nonOpenClosedPrincibleEmployee = new NonOpenClosedPrincibleEmployee();
+nonOpenClosedPrincibleEmployee.GetEmployeeInfo(EmployeeType.Manager);
+
+Console.WriteLine("-------------------------------------------------------------------------------------------------------------------------");
+
+var employee = new Employee();
+employee.GetEmployeeInfo(new Developer());
+
+
+
+public enum EmployeeType
+{
+    Manager,
+    Developer,
+    HumanResources
+}
+public class NonOpenClosedPrincibleEmployee
+{
+    public void GetEmployeeInfo(EmployeeType employeeType)
+    {
+        if(employeeType == EmployeeType.Manager)
+        {
+            Console.WriteLine("NonOpenClosedPrincibleEmployee:Info is pulling for Manager");
+        }
+
+        if (employeeType == EmployeeType.Developer)
+        {
+            Console.WriteLine("NonOpenClosedPrincibleEmployee:Info is pulling for Developer");
+        }
+
+        if (employeeType == EmployeeType.HumanResources)
+        {
+            Console.WriteLine("NonOpenClosedPrincibleEmployee:Info is pulling for Human Resources");
+        }
+    }
+}
+
+public interface IEmployee
+{
+    void GetEmployeeInfo();    
+}
+
+public class Manager :IEmployee
+{
+    public void GetEmployeeInfo()
+    {
+        Console.WriteLine("Info is pulling for Manager");
+
+    }
+}
+
+public class Developer : IEmployee
+{
+    public void GetEmployeeInfo()
+    {
+        Console.WriteLine("Info is pulling for Developer");
+
+    }
+}
+
+public class HumanResources : IEmployee
+{
+    public void GetEmployeeInfo()
+    {
+        Console.WriteLine("Info is pulling for Human Resources");
+
+    }
+}
+
+public class Employee
+{
+    public void GetEmployeeInfo(IEmployee employee)
+    {
+        employee.GetEmployeeInfo();
+    }
+}
