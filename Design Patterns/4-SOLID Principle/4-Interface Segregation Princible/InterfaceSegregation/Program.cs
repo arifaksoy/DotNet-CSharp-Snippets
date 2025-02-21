5dd06ccd0402dc88dd899c5a01af19458c51525c
Arif Aksoy <a.aksoy@cellsol.net>
2025-02-21 15:12:03 +0300

    Added Interface Segregation princible

---
+++ Design Patterns/4-SOLID Principle/4-Interface Segregation Princible/InterfaceSegregation/Program.cs
@@ -0,0 +1,135 @@
+﻿
+
+//non Interface Segregation Princible
+public interface CloudProvider
+{
+    void StoreFile(string name);
+
+    void GetFile(string name);
+
+    void CreateServer();
+
+    void ListServers();
+
+    void GetCDNAddress();
+
+}
+
+public class Amozon : CloudProvider
+{
+    public void CreateServer()
+    {
+        Console.WriteLine("Creating servers");
+    }
+
+    public void GetCDNAddress()
+    {
+        Console.WriteLine("Get CDN Address");
+    }
+
+    public void GetFile(string name)
+    {
+        Console.WriteLine("Getting file");
+    }
+
+    public void ListServers()
+    {
+        Console.WriteLine("Listing servers");
+    }
+
+    public void StoreFile(string name)
+    {
+        Console.WriteLine("Storing File");
+    }
+}
+
+public class DropBox : CloudProvider
+{
+    public void CreateServer()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void GetCDNAddress()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void GetFile(string name)
+    {
+        Console.WriteLine("Getting file");
+    }
+
+    public void ListServers()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void StoreFile(string name)
+    {
+        Console.WriteLine("Storing File");
+    }
+}
+
+
+
+//Interface Segregation Princible
+
+public interface CloudHostingProvider
+{
+    void CreateServer();
+
+    void ListServers();
+}
+
+public interface CDNProvider
+{
+    void GetCDNAddress();
+}
+
+public interface CloudStorageProvider
+{
+    void StoreFile(string name);
+    void GetFile(string name);
+}
+
+public class Amozon2 : CloudHostingProvider,CDNProvider,CloudStorageProvider
+{
+    public void CreateServer()
+    {
+        Console.WriteLine("Creating servers");
+    }
+
+    public void GetCDNAddress()
+    {
+        Console.WriteLine("Get CDN Address");
+    }
+
+    public void GetFile(string name)
+    {
+        Console.WriteLine("Getting file");
+    }
+
+    public void ListServers()
+    {
+        Console.WriteLine("Listing servers");
+    }
+
+    public void StoreFile(string name)
+    {
+        Console.WriteLine("Storing File");
+    }
+}
+
+public class DropBox2 : CloudStorageProvider
+{
+    public void GetFile(string name)
+    {
+        Console.WriteLine("Getting file");
+    }
+
+    public void StoreFile(string name)
+    {
+        Console.WriteLine("Storing File");
+    }
+}
