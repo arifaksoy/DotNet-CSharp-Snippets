9c7ed6e2281bff7bb28ee4dd61e78eeb62a324c7
Arif Aksoy <a.aksoy@cellsol.net>
2025-02-25 17:50:02 +0300

    Added dependency inversion prenscible

---
+++ Design Patterns/4-SOLID Principle/5-Dependency Inversion Princible/DependencyInversion/Program.cs
@@ -0,0 +1,113 @@
+﻿
+
+
+public class MongoDB
+{
+    public void Insert()
+    {
+        //insert data
+    }
+
+    public void Update()
+    {
+        //update data
+    }
+
+    public void Delete()
+    {
+        //delete data
+    }
+}
+
+public class MySQLDatabase
+{
+    public void Insert()
+    {
+        //insert data
+    }
+
+    public void Update()
+    {
+        //update data
+    }
+
+    public void Delete()
+    {
+        //delete data
+    }
+}
+
+public class BudgetReport
+{
+    public void save()
+    {
+        var database = new MySQLDatabase();
+        database.Update();
+    }
+}
+
+
+
+//Dependency Inversion
+
+public interface Database
+{
+    public void Insert();
+
+    public void Update();
+
+    public void Delete();
+  
+}
+
+
+public class MongoDB2:Database
+{
+    public void Insert()
+    {
+        //insert data
+    }
+
+    public void Update()
+    {
+        //update data
+    }
+
+    public void Delete()
+    {
+        //delete data
+    }
+}
+
+public class MySQLDatabase2 : Database
+{
+    public void Insert()
+    {
+        //insert data
+    }
+
+    public void Update()
+    {
+        //update data
+    }
+
+    public void Delete()
+    {
+        //delete data
+    }
+}
+
+public class BudgetReport2
+{
+    private readonly Database _database;
+
+    public BudgetReport2(Database database)
+    {
+        _database = database;
+    }
+    public void save()
+    {
+        _database.Update();
+    }
+}
+
